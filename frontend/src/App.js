4f2ef2a71138487bd1bdd25cb4ee851184f90f71
nimbus117 <mail@nimbus117.co.uk>
2018-11-05 11:34:53 +0000

    added navbar and home components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from './components/navbar'
+import Home from './containers/home'
+// import Hunters from './Hunters'
+import ScrollToTop from 'react-scroll-up'
 
 class App extends Component {
   render() {
     return (
-      <h1>App loaded</h1>
+      <Router>
+        <React.Fragment>
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <ScrollToTop showUnder={160}>
+            <span>TOP</span>
+          </ScrollToTop>
+        </React.Fragment>
+      </Router>
     );
   }
 }
---
+++ frontend/src/containers/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron'
+
+const Home = props => {
+  return(
+    <Jumbotron>
+      <h1>Home Page</h1>
+      <p>Orion's Outlaws management system. Authorized access only!</p>
+    </Jumbotron>
+  )
+}
+export default Home;
