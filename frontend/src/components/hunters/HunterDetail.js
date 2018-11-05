6b2dbcfb270d695d603a8d045dece7d678d1e09e
chloe alcorn <ca.alcorn@outlook.com>
2018-11-05 15:56:57 +0000

    EVERYTHIN WORKING YAY

--- frontend/src/components/hunters/HunterDetail.js
+++ frontend/src/components/hunters/HunterDetail.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Jumbotron from 'react-bootstrap/lib/Jumbotron'
+import Container from 'react-bootstrap/lib/Container'
+import Row from 'react-bootstrap/lib/Row'
+import Image from'react-bootstrap/lib/Image'
+import Col from 'react-bootstrap/lib/Col'
+import ListGroup from 'react-bootstrap/lib/ListGroup'
 
 const HunterDetail = (props) => {
   return(
-    <h1>test detaol</h1>
+    <div>
+      <Jumbotron>
+        <h1>Hunter: {props.data.name}</h1>
+        <p>Orion's Outlaws management system. Authorized access only!</p>
+      </Jumbotron>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <Image src={props.data.imagePath} fluid/>
+          </Col>
+          <Col md={7}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Email Address:{props.data.emailAdress}</ListGroup.Item>
+              <ListGroup.Item>Wallet:{props.data.wallet}</ListGroup.Item>
+              <ListGroup.Item>Alive: {props.data.alive.toString()}</ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
