e0fdf03c8435d2b622b3c0d3d6ef4ffd123f1e89
chloe alcorn <ca.alcorn@outlook.com>
2018-11-05 19:50:00 +0000

    changd front end to show enum status from back end rather than alive true or false

--- frontend/src/components/hunters/HunterDetail.js
+++ frontend/src/components/hunters/HunterDetail.js
@@ -22,7 +22,7 @@ const HunterDetail = (props) => {
             <ListGroup variant="flush">
               <ListGroup.Item>Email Address:{props.data.emailAdress}</ListGroup.Item>
               <ListGroup.Item>Wallet:{props.data.wallet}</ListGroup.Item>
-              <ListGroup.Item>Alive: {props.data.alive.toString()}</ListGroup.Item>
+              <ListGroup.Item>Alive: {props.data.hunterStatus}</ListGroup.Item>
             </ListGroup>
           </Col>
         </Row>
