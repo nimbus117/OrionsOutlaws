34ccf26dcb1659d501d1ed326aee3e00b607eeb1
Laura Nagy <ngylaura@gmail.com>
2018-11-06 11:34:12 +0000

    adds click functionality to the markers

--- frontend/src/containers/maps/GoogleApiWrapper.js
+++ frontend/src/containers/maps/GoogleApiWrapper.js
@@ -1,74 +1,71 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Container from 'react-bootstrap/lib/Container'
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+    onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
   render() {
 
     const style = {
-        height: "50vh",
+        height: "55vh",
         width: "auto",
         margin: "0 1em"
       }
 
     return (
         <div>
-        <Map google={this.props.google} zoom={2} style={style} center={this.props.google.maps.LatLng(37.774929, -122.419416)}>
+        <Map google={this.props.google} zoom={2} style={style} center={this.props.google.maps.LatLng(38.098015, 9.459019)}
+        onClick={this.onMapClicked}>
 
           <Marker onClick={this.onMarkerClick}
                   name={'Current location'} lat={37.774929} lng={-122.419416} />
 
-          {/* <InfoWindow onClose={this.onInfoWindowClose}>
+          <Marker onClick={this.onMarkerClick}
+            title={'The marker`s title will appear as a tooltip.'}
+            name={'SOMA'}
+            position={{lat: 57.778519, lng: -122.405640}} />
+
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
               <div>
                 <h1>{this.state.selectedPlace.name}</h1>
               </div>
-          </InfoWindow> */}
+          </InfoWindow>
+
         </Map>
       </div>
     );
   }
 }
 
-// export class Map extends React.Component {
-//
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevProps.google !== this.props.google) {
-//       this.loadMap();
-//     }
-//   }
-//
-//   componentDidMount() {
-//     this.loadMap();
-//   }
-//
-//   loadMap() {
-//     if (this.props && this.props.google) {
-//       // google is available
-//       const {google} = this.props;
-//       const maps = google.maps;
-//       const mapRef = this.refs.map;
-//       const node = ReactDOM.findDOMNode(mapRef);
-//       let zoom = 14;
-//       let lat = 37.774929;
-//       let lng = -122.419416;
-//       const center = new maps.LatLng(lat, lng);
-//       const mapConfig = Object.assign({}, {
-//         center: center,
-//         zoom: zoom
-//       })
-//       this.map = new maps.Map(node, mapConfig);
-//     }
-//   }
-//
-//   render() {
-//     return (
-//       <div ref='map'>
-//         Loading map...
-//       </div>
-//     )
-//   }
-// }
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAoAwJKfYYsZKkverzYANY8UjrABJdH8lA'
