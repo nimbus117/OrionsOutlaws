985fe9d57ee2aadfa8b380bfff29478356a7fc55
Laura Nagy <ngylaura@gmail.com>
2018-11-07 00:49:59 +0000

    adds custom query for search in hunters

--- backend/src/main/java/uk/co/orionsoutlaws/ManagementSystem/Repositories/HunterRepository/HunterRepository.java
+++ backend/src/main/java/uk/co/orionsoutlaws/ManagementSystem/Repositories/HunterRepository/HunterRepository.java
@@ -1,9 +1,10 @@
 package uk.co.orionsoutlaws.ManagementSystem.Repositories.HunterRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import uk.co.orionsoutlaws.ManagementSystem.Models.Hunter;
 
-@Repository
-public interface HunterRepository extends JpaRepository<Hunter, Long>{
+@RepositoryRestResource
+public interface HunterRepository extends JpaRepository<Hunter, Long>, HunterRepositoryCustom{
 }
